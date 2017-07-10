c1aac747b6b9595f243c72cc4db1493b7ebe8ace
OmarAhmadien <omarahmadien@gmail.com>
2017-07-10 23:37:08 +0300

    Add files via upload

---
+++ Assignment1/feature_extraction.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+def feature1(x):
+    """This feature computes the proportion of black squares to the
+       total number of squares in the grid.
+       Parameters
+       ----------
+       x: 2-dimensional array representing a maze
+       Returns
+       -------
+       feature1_value: type-float 
+       """
+    feature1_value=0.0
+
+    
+    return feature1_value
+
+def feature2(x):
+    """This feature computes the sum of the max of continuous black squares
+       in each row
+       Parameters
+       ----------
+       x: 2-dimensional array representing a maze
+       Returns
+       -------
+       feature2_value: type-float 
+       """
+    feature2_value=0.0
+
+    
+    return feature2_value
+
+    
+    
+
+
